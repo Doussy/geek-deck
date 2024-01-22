130ea4be2b1e8a2ae279393c0c0c4f9f502b92a8
Andre Doussy <doussya@gmail.com>
2024-01-22 13:10:53 +0100

    feat: make the app configurable via a json file

--- geek-deck.py
+++ geek-deck.py
@@ -1,89 +1,30 @@
+import json
 from flask import Flask, jsonify, render_template
 import pyautogui
 import qrcode_terminal
 import socket
 
 app = Flask(__name__)
-    
-@app.route('/stage', methods=['POST'])
-def stage():
-    try:
-        pyautogui.hotkey('command', 'ctrl', 't')
-        return jsonify({'message': 'COMMAND+CONTROL+T keys pressed successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
-@app.route('/microphone', methods=['POST'])
-def camera():
-    try:
-        pyautogui.hotkey('command', 'd')
-        return jsonify({'message': 'COMMAND+D keys pressed successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+def create_endpoint(route, method, keys):
+    def func():
+        try:
+            pyautogui.hotkey(*keys)
+            return jsonify({'message': f'{"+".join(keys).upper()} keys pressed successfully'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    func.__name__ = '_'.join(keys)
+    app.route(route, methods=[method])(func)
 
-@app.route('/camera', methods=['POST'])
-def microphone():
-    try:
-        pyautogui.hotkey('command', 'e')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    
-@app.route('/start_stop_slides', methods=['POST'])
-def start_stop_slides():
-    try:
-        pyautogui.hotkey('command', 'p')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+with open('static/endpoints.json') as f:
+    endpoints = json.load(f)
 
-@app.route('/left_arrow', methods=['POST'])
-def left_arrow():
-    try:
-        pyautogui.hotkey('left')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+for endp in endpoints:
+    create_endpoint(endp['route'], endp['method'], endp['keys'])
 
-@app.route('/right_arrow', methods=['POST'])
-def right_arrow():
-    try:
-        pyautogui.hotkey('right')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    
-@app.route('/grid', methods=['POST'])
-def grid():
-    try:
-        pyautogui.hotkey('command', 'ctrl', 'j')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/floating_tile', methods=['POST'])
-def floating_tile():
-    try:
-        pyautogui.hotkey('command', 'ctrl', 'k')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/active_speaker', methods=['POST'])
-def active_speaker():
-    try:
-        pyautogui.hotkey('command', 'ctrl', 'l')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    
-@app.route('/scene', methods=['POST'])
-def scene():
-    try:
-        pyautogui.hotkey('command', 'v')
-        return jsonify({'message': 'Yes'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/endpoints.json')
+def endpoints():
+    return app.send_static_file('endpoints.json')
     
 @app.route('/deck', methods=['GET'])
 def buttons():
